e9fa78e5b9841bc16663649a26f0410d2cddcda9
Fernando <camargo.fernando@gmail.com>
2024-10-22 08:47:06 -0300

    Ajustes isSidebaropen

--- src/pages/AirSpace/Airspace.component.tsx
+++ src/pages/AirSpace/Airspace.component.tsx
@@ -1,24 +1,32 @@
 import { Link } from "react-router-dom"
 import { AirspaceWrap } from "./Airspace.styles"
+import { BlockContentWrap } from "../../styles/Global/default"
 
 
 export const Airspace = () => {
   return (
-    <AirspaceWrap>
-      <h3>Espaço Aéreo</h3>
-      <div>
-        Desde 1986, o Código Brasileiro de Aeronáutica estabelece que o
-        aerodesporto deverá ser praticado em áreas determinadas pela
-        autoridade aeronáutica. É por isso que a prática só é regular
-        quando realizada dentro do Espaço Aéreo Condicionado (EAC), em
-        áreas geralmente estabelecidas por SBR´s ou NOTAM destinadas ao
-        aerodesporto e disponíveis no site do Departamento de Controle de
-        Espaço Aéreo (DECEA)
+    <AirspaceWrap className='airspace-content'>
+      <BlockContentWrap className="airspace-header">
+        <div className="airspace-title">Espaço aéreo - CPVL</div>
+      </BlockContentWrap>
+
+      <div className='airspace-block'>
+        <h3>Espaço Aéreo</h3>
+        <div>
+          Desde 1986, o Código Brasileiro de Aeronáutica estabelece que o
+          aerodesporto deverá ser praticado em áreas determinadas pela
+          autoridade aeronáutica. É por isso que a prática só é regular
+          quando realizada dentro do Espaço Aéreo Condicionado (EAC), em
+          áreas geralmente estabelecidas por SBR´s ou NOTAM destinadas ao
+          aerodesporto e disponíveis no site do Departamento de Controle de
+          Espaço Aéreo (DECEA)
+        </div>
+
+        <div>
+          <Link to="/airspace">Espaço Aéreo</Link>
+        </div>
       </div>
 
-      <div>
-        <Link to="">Espaço Aéreo</Link>
-      </div>
     </AirspaceWrap>
   )
 }
--- src/pages/AirSpace/Airspace.styles.ts
+++ src/pages/AirSpace/Airspace.styles.ts
@@ -1,5 +1,80 @@
+import { media, theme } from "../../styles/Theme/theme"; 
 import styled from "styled-components";
+import image_airspace from "../../assets/Images/r_norte1.jpg";
 
 export const AirspaceWrap = styled.div`
 
+  .airspace-content {
+    align-items: center;
+    display: flex-wrap;
+    height: 88vh;
+    justify-content: center;
+    width: auto;
+  }
+
+  .airspace-header {
+    align-items: center;
+    background-image: url(${image_airspace});
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    justify-content: center;
+    height: 30vh;
+  }
+
+  .airspace-block {
+    align-items: start;
+    font-family: roboto, sans-serif;    
+    display: flex;
+    justify-content: space-around;
+    height: auto;
+    margin: 50px 0px;
+    text-align: justify;
+
+    ${media.md`
+      display: block;
+    `}
+  }
+  
+  .airspace-title{
+    align-items: center;
+    border: 2px solid ${theme.colors.white};
+    color: ${theme.colors.white};     
+    display: flex; 
+    font-size: 18px;
+    font-weight: 700;
+    justify-content: center;
+    margin-top: -1em;
+    padding: 12px 20px;
+    text-transform: uppercase;
+    width: auto;
+  }
+
+  .airspace-block-img {
+    align-items: center;
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    height: 30vh;
+    justify-content: center;
+    width: 30vw;
+
+    ${media.md`
+      width: 100%;
+    `}
+  }
+
+  .airspace-text{
+    align-items: center;    
+    font-size: 14px;
+    font-weight: 500;
+    justify-content: center;
+    max-width: 600px;
+    line-height: 2;
+    padding: 12px 20px;
+
+    ${media.md`
+      max-width: 100%;
+    `}
+  }
 `
