732e84809c4e9eea2fad44c724c1e753952d06ad
Fernando <camargo.fernando@gmail.com>
2024-10-29 17:06:23 -0300

    Private área

--- src/containers/Dashboard/Dashboard.component.tsx
+++ src/containers/Dashboard/Dashboard.component.tsx
@@ -1,11 +1,3 @@
-// import { useCallback } from 'react';
-// import { useLocalStorage } from 'usehooks-ts';
-// import { NavBar } from '../../components/NavBar/NavBar.component';
-// import { API, getURI } from '../../services';
-// import { IAllowedRoutes } from '../../components/Sidebar/Sidebar.component';
-// import { useFetch } from '../../hooks';
-// import { Outlet, useNavigate } from 'react-router-dom';
-
 export const DashboardHome = () => {
   return (
     <div>Faça seu login para entrar na área restrita.</div>
@@ -13,39 +5,5 @@ export const DashboardHome = () => {
 }
 
 export const Dashboard = () => {
-  // const { data: allowedRoutes } = useFetch<IAllowedRoutes[]>({
-  //   url: getURI(API.profile)
-  // });
 
-  // const [, setIsLogged] = useLocalStorage(
-  //   process.env.REACT_APP_LOGGED_KEY as string,
-  //   false
-  // );
-
-  // const navigate = useNavigate(); 
-
-  // const doLogout = useCallback(() => {
-  //   setIsLogged(false);
-  //   navigate('/');
-  // }, [setIsLogged]);
-
-  // const navTo = useCallback(
-  //   (link: IAllowedRoutes) => {
-  //     navigate(link.route);
-  //   },
-  //   [navigate]
-  // );
-
-  // if (!allowedRoutes) return null;
-
-  // return (
-  //   <>
-  //     <NavBar
-  //       allowedRoutes={allowedRoutes}
-  //       onLogout={doLogout}
-  //       onNav={navTo}
-  //     ></NavBar>
-  //     <Outlet />
-  //   </>
-  // );
 };
