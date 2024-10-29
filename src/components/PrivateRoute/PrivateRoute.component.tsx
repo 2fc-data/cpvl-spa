ef1224dbac7d841770dbcb2896f819b6e50d59d9
Fernando <camargo.fernando@gmail.com>
2024-10-29 17:29:30 -0300

    Removido conexão com api

--- src/components/PrivateRoute/PrivateRoute.component.tsx
+++
@@ -1,6 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-import { useLocalStorage } from "usehooks-ts";
-export const PrivateRoute = () => {
-  const [isLogged] = useLocalStorage(process.env.REACT_APP_LOGGED_KEY, false);
-  return !Boolean(isLogged) ? <Navigate to="/Home" /> : <Outlet />;
-}
