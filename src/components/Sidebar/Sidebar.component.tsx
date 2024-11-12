9e9ddb3ad8dcbc957fab227b58aa7d3e28ac5a89
Fernando <camargo.fernando@gmail.com>
2024-11-12 14:27:00 -0300

    variável de ambiente no vite é assim: import.meta.env.VITE_REACT_APP_LOGGED_KEY

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -5,9 +5,10 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setSidebarState } from "../../redux/Slices/SidebarSlice";
 import { RootState, AppDispatch } from "../../redux/Store";
+import { togglePrivateMenu } from "../../redux/Slices/PrivateMenuSlice";
 
 import { useLocalStorage } from 'usehooks-ts';
-import { useState } from "react";
+// import { useState } from "react";
 
 export interface IAllowedRoutes {
   label: string;
@@ -23,34 +24,36 @@ interface IProps {
 export const Sidebar = ({ onLogout, onNav, allowedRoutes }: IProps) => {
   const dispatch: AppDispatch = useDispatch();
   const isSidebarOpen = useSelector((state: RootState) => state.sidebar.isSidebarOpen);
+  const isPrivateMenuOpen = useSelector((state: RootState) => state.privateMenu.isPrivateMenuOpen);  
 
   const navigate = useNavigate();
-  const [isLogged] = useLocalStorage(process.env.REACT_APP_LOGGED_KEY!, false);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [isPilotMenuOpened, setIsPilotMenuOpened] = useState(false);
-  const [isPublicMenuOpened, setIsPublicMenuOpened] = useState(false);
+  const [isLogged] = useLocalStorage(import.meta.env.REACT_APP_LOGGED_KEY!, false);
+  // const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  // const [isPrivateMenuOpened, setIsPrivateMenuOpened] = useState(false);
+  // const [isPublicMenuOpened, setIsPublicMenuOpened] = useState(false);
 
-  const handlePublicMenuClick = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    setAnchorEl(event.currentTarget);
-    setIsPublicMenuOpened(true);
-  };
+  
+  // const handlePublicMenuClick = (
+  //   event: React.MouseEvent<HTMLButtonElement>
+  // ) => {
+  //   setAnchorEl(event.currentTarget);
+  //   setIsPublicMenuOpened(true);
+  // };
 
-  const handlePilotMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-    setIsPilotMenuOpened(true);
-  };
+  // const handlePrivateMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   setAnchorEl(event.currentTarget);
+  //   setIsPrivateMenuOpened(true);
+  // };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-    setIsPilotMenuOpened(false);
-    setIsPublicMenuOpened(false);
-  };
+  // const handleClose = () => {
+  //   setAnchorEl(null);
+  //   setIsPrivateMenuOpened(false);
+  //   setIsPublicMenuOpened(false);
+  // };
 
   const handleItemClick = (route: IAllowedRoutes) => {
     onNav(route);
-    handleClose();
+    // handleClose();
   };
 
   return (
@@ -61,13 +64,13 @@ export const Sidebar = ({ onLogout, onNav, allowedRoutes }: IProps) => {
             type="button"
             className="sidebar-close-btn"
             onClick={() => dispatch(setSidebarState(false))}
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              horizontal: 'right',
-              vertical: 'bottom'
-            }}
-            onClose={handleClose}
-            open={isPublicMenuOpened}
+            // anchorEl={anchorEl}
+            // anchorOrigin={{
+            //   horizontal: 'right',
+            //   vertical: 'bottom'
+            // }}
+            // onClose={handleClose}
+            // open={isPublicMenuOpened}
           >
             <MdClose size={45} />
           </button>
@@ -116,10 +119,10 @@ export const Sidebar = ({ onLogout, onNav, allowedRoutes }: IProps) => {
                 <button
                   type="button"
                   className="sidenav-link"
-                  onClick={(handleClose) => navigate('/login')}
+                  // onClick={(handleClose) => navigate('/login')}
                 >
                   <VscSignOut size={30} />
-                  <span className="link-text">Sair</span>
+                  <span className="link-text">Login</span>
                 </button>
               </li>
             )}
@@ -127,16 +130,16 @@ export const Sidebar = ({ onLogout, onNav, allowedRoutes }: IProps) => {
               <>
                 <button
                   type="button"
-                  onClick={handlePilotMenuClick}
+                  // onClick={handlePrivateMenuClick}
                 >
-                  Restrito
+                  Restrito                  
                 </button>
 
                 <ul
-                  id="main-menu"          
-                  anchorEl={anchorEl}
-                  open={isPilotMenuOpened}
-                  onClose={handleClose}
+                  // id="main-menu"          
+                  // anchorEl={anchorEl}
+                  // open={isPrivateMenuOpened}
+                  // onClose={handleClose}
                 >
                   {allowedRoutes.map((route, idX) => (
                     <li key={idX} onClick={() => handleItemClick(route)}>
@@ -160,34 +163,6 @@ export const Sidebar = ({ onLogout, onNav, allowedRoutes }: IProps) => {
                 </li>
               </>
             )}
-
-
-
-
-
-
-
-
-
-
-            {/* {["home", "about",  "direction", "login", "logout"].map((item, index) => (
-              <li key={index} className="sidenav-item">
-                <Link
-                  className="sidenav-link"
-                  to={`/${item.toLowerCase()}`}
-                  onClick={() => dispatch(setSidebarState(false))}
-                >
-                  <span className="link-icon">
-                    {index === 0 && <MdOutlineHome size={30} />}
-                    {index === 1 && <MdOutlineSatelliteAlt size={30} />}
-                    {index === 2 && <MdOutlineGroups size={30} />}
-                    {index === 3 && <VscSignOut size={30} />}
-                    {index === 4 && <VscSignIn size={30} />}
-                  </span>
-                  <span className="link-text">{item}</span>
-                </Link>
-              </li>
-            ))} */}
           </ul>
         </nav>
 
